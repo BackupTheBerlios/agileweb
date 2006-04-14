3c669d091b325ad6602c9508714ee0b5a876fcd4
nio_xiao <nio_xiao>
2006-04-14 05:40:00 +0000

    diggmore base Exception.

---
+++ diggmore/src/lib/DiggmoreException.php
@@ -0,0 +1,9 @@
+<?php
+//  $Id: DiggmoreException.php,v 1.1 2006/04/14 05:40:00 nio_xiao Exp $
+
+/**
+ * @package     DiggMore
+ * @copyright   Copyright (c) 2006 PHP More. (http://www.phpmore.com)
+ */
+class DiggmoreException extends Exception
+{}
