9ae5aa2884938236fc1ffc6a853f9d6ead163404
binzywu <binzywu>
2006-04-18 01:30:54 +0000

    *** empty log message ***

---
+++ diggmore/src/lib/ext/SubDirectoryRouter.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'Zend/Controller/Router/Interface.php';
+
+class SubDirectoryRouter  implements Zend_Controller_Router_Interface
+{
+	//
+
+	public function route(Zend_Controller_Dispatcher_Interface $dispatcher)
+	{
+
+	}
+
+}
+
+
+?>
